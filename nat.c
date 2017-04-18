b8bc01259b1bec0bbde2869ba298ab23e77478cf
kkkio <kevin.l.yide@gmail.com>
2017-04-18 21:20:56 +0800

    complete main

--- nat.c
+++ nat.c
@@ -23,17 +23,19 @@ nfqnl_msg_packet_hdr *ph;
 uint32_t publicIP;
 uint32_t lanIP;
 uint32_t subnetMask;
+uint32_t subnetIP;
 unsigned int mask = 0xFFFFFFFF; //255
 int packet_num = 0;
 
 
 int main(int argc, char ** argv){
+  struct nfq_handle *h;
   struct nfq_q_handle *qh;
   struct nfnl_handle *nh;
-  struct nfq_handle *h;
-  int fd,rv;
+  int fd,len;
   char buf[4096];
   struct in_addr* inp = (in_addr*)malloc(sizeof(in_addr));
+  int mask_int;
 
   if(argc!=4){
     printf("Usage: ./nat <public ip> <internal ip> <subnet mask>\n");
@@ -45,6 +47,35 @@ int main(int argc, char ** argv){
   publicIP = ntohl(inp->s_addr); // publicIP
   inet_aton(argv[2],inp);
   lanIP = ntohl(inp->s_addr);    // lanIP
-  mask
+  mask_int = atoi(argv[3]);
+  subnetMask  = mask << (32-mask_int);
+  subnetIP = lanIP & subnetMask;       // local_network(localIP);
 
+  // Open library handle
+  if(!(h = nfq_open())){
+    fprintf(stderr, "Error: nfq_open()\n");
+    exit(-1);
+  }
+  if(nfq_bind_pf(h,AF_INET)<0){
+    fprintf(stderr, "Error: nfq_bind_pf()\n");
+    exit(-1);
+  }
+  if(!(qh = nfq_create_queue(h,0,&Callback,NULL))){
+    fprintf(stderr, "Error: nfq_create_queue()\n");
+    exit(-1);
+  }
+  if(nfq_set_mode(qh,NFQNL_COPY_PACKET, 0XFFFF)<0){
+    fprintf(stderr, "Error: Could not set packet copy mode\n");
+    exit(-1);
+  }
+  fd = nfq_fd(h);
+  while((len = recv(fd,buf,sizeof(buf),0)) && len >= 0){
+    nfq_handle_packet(h,buf,len);
+  }
+
+  // cleanup data structure
+  printf("unbinding from queue 0\n");
+  nfq_destroy_queue(qh);
+  nfq_close(h);
+  return 0;
 }
