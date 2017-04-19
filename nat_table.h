e0a441eaa58c521f294db0cb438427e76211a463
= <chengtsung_liu@yahoo.com>
2017-04-19 16:12:39 +0800

    add tcp_state definition

--- nat_table.h
+++ nat_table.h
@@ -2,6 +2,13 @@
 //   Translated port
 //   Internal address pair, i.e IP and port
 //   TCP state
+#define ACTIVE 0
+#define FIN1_SENT 1
+#define FIN1_RECEIVED 2
+#define ACK1_SENT 3
+#define ACK1_RECEIVED 4
+#define FIN2_SENT 5
+#define FIN2_RECEIVED 6
 
 
 // struct for enrty
@@ -9,7 +16,7 @@ typedef struct nat_entry{
     unsigned short t_port;          //translated port
     unsigned short i_port;          //Internal port
     unsigned long i_addr;           //Internal IP
-    char *tcp_state;                //TCP state ??????
+    int tcp_state;                //TCP state ??????
 }nat_e;
 
 // struct for table
@@ -19,7 +26,7 @@ nat_e **entry_list;
 
 //Desirable functions
 
-nat_t *create_table();      //create a new translation entry
+nat_e **create_table();      //create a new translation entry
 nat_e *searchSource(nat_e **table, unsigned long addr, unsigned short port);
 nat_e *searchDest(nat_e **table, unsigned short port);
 nat_e *insert(nat_e **table, unsigned long addr, unsigned short port);
